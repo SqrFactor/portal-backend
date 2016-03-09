7a66f721c3d8ba02f7418c35dd0fe448d4073c1b
Angad Gill <angad.cec@gmail.com>
2016-03-09 14:12:12 +0530

    Added user type data into sql query

--- DBDesign/DBQueryFile.sql
+++ DBDesign/DBQueryFile.sql
@@ -438,6 +438,11 @@ userType        varchar(20)  NOT NULL,
 PRIMARY KEY (userTypeId)
 );
 
+INSERT INTO `sqr_factor`.`mstr_user_type` (`userTypeId`, `userType`) VALUES ('101', 'Student');
+INSERT INTO `sqr_factor`.`mstr_user_type` (`userTypeId`, `userType`) VALUES ('102', 'Professional');
+INSERT INTO `sqr_factor`.`mstr_user_type` (`userTypeId`, `userType`) VALUES ('103', 'Teacher');
+INSERT INTO `sqr_factor`.`mstr_user_type` (`userTypeId`, `userType`) VALUES ('104', 'Admin');
+
 Create table user_details(
 userId       int(16) AUTO_INCREMENT  NOT NULL,
 firstName    varchar(100)  NOT NULL,
